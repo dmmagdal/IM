f2a1767ea894eb30b6aca4053d5b554c44f174fd
Diego Magdaleno <dmmagdal@ucsc.edu>
2017-05-20 20:58:48 -0700

    starting php based web chat room

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+<title>Chat Room</title>
+</head>
+
+<body align="center" valign"middle">
+
+<form action="chatroom.php" method="POST">
+<input type="text" name="username"/>
+<input type="submit" name="Enter"/>	
+
+
+
+</form>
+</body>
+
+</html>
